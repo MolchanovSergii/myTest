6a05caaeabacdfa2185815933d98418a7cae0496
MolchanovSergii <molchanse@gmail.com>
2023-03-16 20:04:06 +0200

    finish

--- js/test.js
+++ js/test.js
@@ -1,13 +1,4 @@
 
-// - Удалить из массива ученых, родившихся в 15, 16 или 17 веках.
-// - Найти ученого, который родился позже всех.
-// - Найти год рождения Albert Einstein.
-// - Найти ученых, фамилия которых начинается на букву "С".
-// - Удалить из массива всех ученых, имя которых начинается на букву "A".
-
-// - Найти ученых, у которых совпадают первые буквы имени и фамилии.
-// - Узнать, все ли ученые работали в 19 веке.
-
 const arr = [
 { name: "Albert", surname: "Einstein", born: 1879, dead: 1955, id: 1 }, 
 { name: "Isaac", surname: "Newton", born: 1643, dead: 1727, id: 2 },
@@ -51,3 +42,44 @@ console.log(arr);
 // - Найти ученого, который прожил больше всех и ученого, который меньше.
 console.log(`ученый, который прожил меньше всех это ${arr[0].surname}, который прожил ${arr[0].live}, 
 а больше всх прожил ${arr[arr.length - 1].surname}, который умер в возрасте ${arr[arr.length - 1].live} лет`);
+
+// - Найти ученого, который родился позже всех.
+arr.sort((a, b) => a.born - b.born);
+console.log(arr);
+console.log(`ученый, который родился позже всех это ${arr[arr.length - 1].name} ${arr[arr.length - 1].surname}`);
+
+// - Найти год рождения Albert Einstein.
+const dayEin = arr.findIndex(element => element.surname === 'Einstein');
+console.log(`Einstein vas born ${arr[dayEin].born} year`);
+
+// - Найти ученых, фамилия которых начинается на букву "С".
+const nameSince = arr.filter((element) => element.surname.startsWith('C'));
+console.log(nameSince);
+
+// - Удалить из массива всех ученых, имя которых начинается на букву "A".
+const nameSince2 = arr.filter((element) => !element.name.startsWith('A'));
+console.log(nameSince2);
+
+// - Узнать, все ли ученые работали в 19 веке.
+const know = arr.every((element) => element.dead <= 1899) ? "есть ученные, которые работали в 19 в" : "таких ученных нет";
+console.log(know);
+
+// - Найти ученых, у которых совпадают первые буквы имени и фамилии.
+const nameSince3 = arr.filter((element) => element.name.charAt(0) === element.surname.charAt(0));
+console.log(nameSince3);
+
+// - Удалить из массива ученых, родившихся в 15, 16 или 17 веках.
+const nameSince4 = arr.filter(element => element.born > 1699);
+console.log(nameSince4);
+
+
+
+
+
+
+
+
+
+
+
+
